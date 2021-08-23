af0e83e6eadfb1e0a8327147bcc3c8baf96bc9ba
Pantkowsky <a.pantkowski@gmail.com>
2021-08-23 23:09:51 +0200

    added `chart.svelte`

---
+++ src/lib/chart/Chart.svelte
@@ -0,0 +1,36 @@
+<script>
+    export let entries
+
+</script>
+
+<div class="container">
+   {#each entries as entry}
+    <div class="bar" style="height:{entry.height}%; width:{entry.width}%"/>
+    <!-- <div class="bar" style="height:{entry}%;"/> -->
+   {/each}
+</div>
+
+<style>
+
+    .container {
+        width: 100%;
+        height: 600em;
+        background-color: white;
+        display: flex;
+		flex-direction: row;
+		justify-content: left;
+        align-items: bottom;
+        position: relative;
+		flex: 1;
+    }
+
+    .bar {
+        width: 1%;
+        margin-top: 3px;
+        margin-left: 1px;
+        background-color: rgb(109, 158, 250);
+        position: relative;
+        align-self: baseline;
+        display: flex;
+    }
+</style>
