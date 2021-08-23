40d7951ee55ccf66075cd073e72b6bbf8ba7cb30
Pantkowsky <a.pantkowski@gmail.com>
2021-08-23 23:10:09 +0200

    added `chooser.svelte`

---
+++ src/lib/chooser/Chooser.svelte
@@ -0,0 +1,26 @@
+<script>
+    let active = ''
+
+</script>
+
+<div>
+    <button class:active="{ active === 'quicksort' }" on:click="{() => active = 'quicksort'}">QuickSort</button>
+    <button class:active="{ active === 'mergesort' }" on:click="{() => active = 'mergesort'}">MergeSort</button>
+    <button class:active="{ active === 'bubblesort' }" on:click="{() => active = 'bubblesort'}">BubbleSort</button>
+    <button class:active="{ active === 'selectionsort' }" on:click="{() => active = 'selectionsort'}">SelectionSort</button>
+</div>
+
+<style>
+
+    div {
+        width: 100%;
+        position: relative;
+		height: 0;
+		padding: 0 0 calc(100% * 100 / 2048) 0;
+    }
+
+    .active {
+        background-color: coral;
+        color: cornsilk;
+    }
+</style>
