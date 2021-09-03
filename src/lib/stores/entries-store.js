d73cd276b984b019d94d53b94cb1c747a054aed9
Pantkowsky <a.pantkowski@gmail.com>
2021-09-03 23:43:09 +0200

    moved shuffling logic to the store

--- src/lib/stores/entries-store.js
+++ src/lib/stores/entries-store.js
@@ -1,4 +1,23 @@
 import { writable } from 'svelte/store'
 
-export let entries = writable({})
-export let ids = writable({})
+function createArray() {
+
+    const array = [...Array(200).keys()]
+    const { subscribe, set, update } = writable(array)
+
+    return {
+        subscribe,
+        shuffle: () => set(shuffle([...Array(200).keys()])),
+        reset: () => set(array)
+    }
+}
+
+function shuffle(a) {
+    for (let i = a.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [a[i], a[j]] = [a[j], a[i]];
+    }
+    return a
+}
+
+export let entries = createArray()
